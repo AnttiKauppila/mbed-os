eff85a58a3672a417dfd4a73e66e8cc15a740346
Antti Kauppila <antti.kauppila@arm.com>
2019-07-03 09:04:42 +0300

    randlib hack for bare-metal

--- features/frameworks/mbed-client-randlib/source/randLIB.c
+++ features/frameworks/mbed-client-randlib/source/randLIB.c
@@ -16,7 +16,38 @@
 #include <stdint.h>
 #include <limits.h>
 #include "randLIB.h"
+#ifdef MBED_CONF_RTOS_PRESENT
 #include "platform/arm_hal_random.h"
+#else
+#include "mbedtls/entropy_poll.h"
+
+#ifdef MBEDTLS_ENTROPY_HARDWARE_ALT
+#if defined(MBEDTLS_PLATFORM_C)
+#include "mbedtls/platform.h"
+#endif
+#endif
+
+void arm_random_module_init(void)
+{
+}
+
+uint32_t arm_random_seed_get(void)
+{
+    uint32_t result = 0;
+#ifdef MBEDTLS_ENTROPY_HARDWARE_ALT
+#if defined(MBEDTLS_PLATFORM_C)
+    mbedtls_platform_setup(NULL);
+#endif /* MBEDTLS_PLATFORM_C */
+    /* Grab a seed from a function we provide for mbedtls */
+    size_t len;
+    mbedtls_hardware_poll(NULL, (uint8_t *) &result, sizeof result, &len);
+#if defined(MBEDTLS_PLATFORM_C)
+    mbedtls_platform_teardown(NULL);
+#endif /* MBEDTLS_PLATFORM_C */
+#endif
+    return result;
+}
+#endif
 
 /**
  * This library is made for getting random numbers for timing needs in
